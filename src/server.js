e18a7e76f02e636008dd17f66d71556a3171c4f6
gabriel hertzan <gabriel.hertzan@gmail.com>
2026-01-07 11:16:33 -0300

    feat: Add Swagger/OpenAPI documentation for user management and API visualization.

--- src/server.js
+++ src/server.js
@@ -8,16 +8,31 @@ import adoptionRouter from "./routes/adoptionRoutes.js";
 import sessionRouter from "./routes/sessionRoutes.js";
 import mockingRoutes from "./routes/mocksRoutes.js";
 import { __dirname, join } from "./utils/utils.js";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUiExpress from "swagger-ui-express";
 
 const app = express();
 
 app.set("PORT", envs.API_PORT);
 
 /* MIDDLEWARES */
+const swaggerOptions = {
+	definition: {
+		openapi: "3.0.1",
+		info: {
+			title: "Documentación de la app Adoptme BackEnd",
+			description: "API, para manejar la app (USUARIOS-MASCOTAS-ADOPCIONES)",
+		},
+	},
+	apis: [`${__dirname}/docs/**/*.yaml`],
+};
+
+const specs = swaggerJSDoc(swaggerOptions);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(join(__dirname, "../public")));
+app.use("/apidocs", swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
 
 app.use("/api/pets", petRouter);
 app.use("/api/users", userRouter);
